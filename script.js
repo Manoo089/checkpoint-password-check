91280c09fd33ea796da720d0a6d28945f2c53a57
Manuel Hohenadl <manoo089@googlemail.com>
2022-01-10 09:46:20 +0100

    add: PasswordChecker-Class and create needed functions

---
+++ script.js
@@ -0,0 +1,14 @@
+class PasswordChecker {
+    state = {
+        input1: "",
+        input2: "",
+        button: "show",
+    };
+
+    constructor() {}
+
+    initEventHandlers = () => {};
+    handleInputEvent = () => {};
+
+    render = () => {};
+}
