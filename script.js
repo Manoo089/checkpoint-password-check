b6826d09458b997a6b0117cc325da5cf8e220d29
Manuel Hohenadl <manoo089@googlemail.com>
2022-01-11 10:04:01 +0100

    refactor

--- script.js
+++ script.js
@@ -1,123 +1,86 @@
-class PasswordChecker {
-    state = {
-        password: "",
-        passwordConfirm: "",
-        buttonText: "Show Password",
-        lowerCase: false,
-        upperCase: false,
-    };
+const toggleButton = document.querySelector("#toggle-button");
+const password = document.querySelector("#password");
+const passwordConfirm = document.querySelector("#password-confirm");
+const passwordForm = document.querySelector("#form");
+const statusIsEqual = document.querySelector("#status-is-equal");
+const statusLowerCase = document.querySelector("#status-lower-case");
+const statusUpperCase = document.querySelector("#status-upper-case");
+const statusNumberCase = document.querySelector("#status-number-case");
+const statusCharactersCase = document.querySelector("#status-characters-case");
 
-    constructor() {
-        this.initEventHandlers();
-        this.render();
+toggleButton.addEventListener("click", toggleShowHide);
+
+passwordForm.addEventListener("input", function() {
+    if (checkIsEqual(password, passwordConfirm)) {
+        statusIsEqual.textContent = "✅";
+    } else {
+        statusIsEqual.textContent = "❌";
     }
 
-    initEventHandlers = () => {
-        document
-            .querySelector("#input-fields")
-            .addEventListener("input", this.handleInputEvent);
+    if (checkIsLowerCase(password)) {
+        statusLowerCase.textContent = "✅";
+    } else {
+        statusLowerCase.textContent = "❌";
+    }
 
-        document
-            .querySelector("#toggle_button")
-            .addEventListener("click", this.handleToggleShowHide);
-    };
+    if (checkIsUpperCase(password)) {
+        statusUpperCase.textContent = "✅";
+    } else {
+        statusUpperCase.textContent = "❌";
+    }
 
-    handleInputEvent = () => {
-        const inputFirstElement = document.querySelector("#password");
-        const inputSecondElement = document.querySelector("#password-confirm");
-        const inputFirstElementValue = inputFirstElement.value;
-        const inputSecondElementValue = inputSecondElement.value;
-        this.state.password = inputFirstElementValue;
-        this.state.passwordConfirm = inputSecondElementValue;
-        this.checkIsEqual();
-        this.checkToLowerCase();
-        this.checkToUpperCase();
-        this.checkToNumbers();
-        this.checkToCharacters();
-    };
+    if (checkIsNumber(password)) {
+        statusNumberCase.textContent = "✅";
+    } else {
+        statusNumberCase.textContent = "❌";
+    }
 
-    handleToggleShowHide = () => {
-        const inputFirstElement = document.querySelector("#password");
-        const inputSecondElement = document.querySelector("#password-confirm");
+    if (checkCharacters(password)) {
+        statusCharactersCase.textContent = "✅";
+    } else {
+        statusCharactersCase.textContent = "❌";
+    }
+});
 
-        if (this.state.buttonText === "Show Password") {
-            this.state.buttonText = "Hide Password";
-            inputFirstElement.setAttribute("type", "text");
-            inputSecondElement.setAttribute("type", "text");
-        } else {
-            this.state.buttonText = "Show Password";
-            inputFirstElement.setAttribute("type", "password");
-            inputSecondElement.setAttribute("type", "password");
-        }
-        this.render();
-    };
-
-    checkIsEqual = () => {
-        if (
-            this.state.password === this.state.passwordConfirm &&
-            this.state.password !== ""
-        ) {
-            const statusIsEqual = document.querySelector("#is-equal");
-            statusIsEqual.innerText = "✅";
-        } else {
-            const statusIsEqual = document.querySelector("#is-equal");
-            statusIsEqual.innerText = "❌";
-        }
-    };
-
-    checkToLowerCase = () => {
-        const regex = /[a-z]/.test(this.state.password);
-
-        if (regex) {
-            const statusLowerCase = document.querySelector("#status-lower-case");
-            statusLowerCase.innerText = "✅";
-        } else {
-            const statusLowerCase = document.querySelector("#status-lower-case");
-            statusLowerCase.innerText = "❌";
-        }
-    };
-
-    checkToUpperCase = () => {
-        const regex = /[A-Z]/.test(this.state.password);
-
-        if (regex) {
-            const statusUpperCase = document.querySelector("#status-upper-case");
-            statusUpperCase.innerText = "✅";
-        } else {
-            const statusUpperCase = document.querySelector("#status-upper-case");
-            statusUpperCase.innerText = "❌";
-        }
-    };
-
-    checkToNumbers = () => {
-        const regex = /[\d+]/.test(this.state.password);
-
-        if (regex) {
-            const statusNumberCase = document.querySelector("#status-number-case");
-            statusNumberCase.innerText = "✅";
-        } else {
-            const statusNumberCase = document.querySelector("#status-number-case");
-            statusNumberCase.innerText = "❌";
-        }
-    };
-
-    checkToCharacters = () => {
-        if (this.state.password.length >= 10) {
-            const statusCharacterCase = document.querySelector(
-                "#status-characters-case"
-            );
-            statusCharacterCase.innerText = "✅";
-        } else {
-            const statusCharacterCase = document.querySelector(
-                "#status-characters-case"
-            );
-            statusCharacterCase.innerText = "❌";
-        }
-    };
-
-    render = () => {
-        document.querySelector("#toggle_button").innerHTML = this.state.buttonText;
-    };
+function checkIsEqual(password, passwordConfirm) {
+    if (
+        password.value === passwordConfirm.value &&
+        password.value + passwordConfirm.value !== ""
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-const app = new PasswordChecker();
+function checkIsLowerCase(ele) {
+    return /[a-z]/.test(ele.value);
+}
+
+function checkIsUpperCase(ele) {
+    return /[A-Z]/.test(ele.value);
+}
+
+function checkIsNumber(ele) {
+    return /[\d+]/.test(ele.value);
+}
+
+function checkCharacters(ele) {
+    if (ele.value.length >= 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function toggleShowHide() {
+    if (password.type === "password" && passwordConfirm.type === "password") {
+        password.type = "text";
+        passwordConfirm.type = "text";
+        toggleButton.textContent = "Hide Password";
+    } else {
+        password.type = "password";
+        passwordConfirm.type = "password";
+        toggleButton.textContent = "Show Password";
+    }
+}
